f1a439dc1f9fe2543f2ea4140af5db3217166157
Dylan Bohlender <dylan.bohlender@gmail.com>
2022-11-07 22:58:56 -0700

    Intern restrictions on Pathfinder releases channel

--- events/threadCreate.js
+++ events/threadCreate.js
@@ -2,6 +2,7 @@ const {
   HALOFUNTIME_LFG_CHANNEL_ID,
   HALOFUNTIME_MCC_LFG_CHANNEL_ID,
   HALOFUNTIME_WELCOME_CHANNEL_ID,
+  PATHFINDERS_RELEASES_CHANNEL_ID,
 } = require("../constants.js");
 
 async function attemptWelcome(thread) {
@@ -36,10 +37,51 @@ async function attemptLfgHelp(thread) {
   }
 }
 
+async function attemptReleasesEnforcement(thread) {
+  // Do not enforce post restrictions if the thread is not in the Releases channel
+  if (thread.parentId !== PATHFINDERS_RELEASES_CHANNEL_ID) return;
+  let starterMessage;
+  while (!starterMessage) {
+    starterMessage = await thread
+      .fetchStarterMessage({ force: true })
+      .catch(() => null);
+    console.log(`Message: ${starterMessage}`);
+    await new Promise((r) => setTimeout(r, 1000));
+  }
+  const missingPictureAttachment =
+    starterMessage.attachments.size === 0 ||
+    starterMessage.attachments.reduce((acc, attachment) => {
+      if (acc) return true;
+      return !(
+        attachment.contentType.includes("image") ||
+        attachment.contentType.includes("video")
+      );
+    }, false);
+  const missingHaloWaypointLink = true; // TODO: Update this once we know what HaloWaypoint links look like
+  console.log(`Missing Picture: ${missingPictureAttachment}`);
+  console.log(`Missing Link: ${missingHaloWaypointLink}`);
+  const rulesViolated = missingPictureAttachment || missingHaloWaypointLink;
+  if (rulesViolated) {
+    await thread.send({
+      content:
+        `<@${thread.ownerId}>, this post has been automatically closed because it did not follow the guidelines.\n\n` +
+        `**Posts in <#${thread.parentId}> must:**\n` +
+        "1) Have at least one image or video attached\n" +
+        "2) Contain a valid link to a file on Halo Waypoint\n\n" +
+        "This post will be archived when you navigate away from it. It'll be our little secret.",
+    });
+    await thread.setLocked(true, "Did not follow submission rules.");
+    await thread.setArchived(true, "Did not follow submission rules.");
+  } else {
+    // TODO: Send a message to the thread that clearly lists the file ID and posts a [BOOKMARK HERE]() hyperlink to it
+  }
+}
+
 module.exports = {
   name: "threadCreate",
   async execute(thread) {
     await attemptWelcome(thread);
     await attemptLfgHelp(thread);
+    await attemptReleasesEnforcement(thread);
   },
 };
