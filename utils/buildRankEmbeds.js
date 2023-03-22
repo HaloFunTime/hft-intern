3664682c6245462584dc6b45be490c2580c1bfd2
michaelwdanko <michaeldanko@icloud.com>
2023-03-22 08:01:51 -0400

    Formatting changes

--- utils/buildRankEmbeds.js
+++ utils/buildRankEmbeds.js
@@ -1,42 +1,41 @@
 const { EmbedBuilder } = require("discord.js");
 
+// Receive the response from the request in getCSR.js
+// Return an array of Discord embed objects that are formatted to display an Xbox Live Gamertag along with the Halo Infinite CSR ranks for each playlist
 function buildRankEmbeds(csr_data) {
-  const rankEmbeds = [];
-  console.log(`Now running function "buildRankEmbeds"`)
-  // console.log(`About to log value of csr_data that was passed into the function:`)
-  // console.log(csr_data)
-
+  const rank_embeds = [];
 
   for (let playlist of csr_data.playlists) {
     const current_csr = playlist.current.csr;
     if (current_csr !== -1) {
       const currentTierDescription = playlist.current.tierDescription;
-      const imageName = currentTierDescription.replace(' ', '-').toLowerCase();
+      const imageName = currentTierDescription.replace(" ", "-").toLowerCase();
       const rankEmbed = new EmbedBuilder()
         .setTitle(playlist.playlistName)
-        .setThumbnail(`https://api.halofuntime.com/static/csr_images/${imageName}.png`)
+        .setThumbnail(
+          `https://api.halofuntime.com/static/csr_images/${imageName}.png`
+        )
         .addFields({
           name: `${currentTierDescription} (${current_csr})`,
           value: `**GT:** \`${csr_data.gamertag}\``,
         })
         .setTimestamp()
-        .setFooter({ text: 'Current CSR' });
-      rankEmbeds.push(rankEmbed);
+        .setFooter({ text: "Current CSR" });
+      rank_embeds.push(rankEmbed);
     }
   }
-  if (rankEmbeds.length === 0) {
-      let description = `\`${csr_data.gamertag}\` is currently unranked.`;
-      if (csr_data.gamertag === "HFT Intern") {
-        description +=
-          "\n\nThis is because quantifying my greatness is impossible. You mortals wouldn't understand.";
-      }
-      rankEmbeds.push(
-        new EmbedBuilder().setTitle("Unranked").setDescription(description)
-      );
+  if (rank_embeds.length === 0) {
+    let description = `\`${csr_data.gamertag}\` is currently unranked.`;
+    if (csr_data.gamertag === "HFT Intern") {
+      description +=
+        "\n\nThis is because quantifying my greatness is impossible. You mortals wouldn't understand.";
     }
-
-    return rankEmbeds;
-
+    rank_embeds.push(
+      new EmbedBuilder().setTitle("Unranked").setDescription(description)
+    );
   }
-  
-  module.exports = buildRankEmbeds;
+
+  return rank_embeds;
+}
+
+module.exports = buildRankEmbeds;
--- utils/getCSR.js
+++ utils/getCSR.js
@@ -1,30 +1,30 @@
 const axios = require("axios");
 
+//Receive an Xbox Live Gamertag and make a request to the halo-infinite app to determine the CSR
 async function getCSR(xboxLiveGamertag) {
-    console.log("Running getCSR function")
-    const { HALOFUNTIME_API_KEY, HALOFUNTIME_API_URL } = process.env;
-    
-    try {
-        console.log(`getCSR - Making GET request to /halo-infinite/csr?gamertag with ${xboxLiveGamertag}`)
-        const response = await axios.get(
-        `${HALOFUNTIME_API_URL}/halo-infinite/csr?gamertag=${encodeURIComponent(xboxLiveGamertag)}`,
-        {
-            headers: {
-            Authorization: `Bearer ${HALOFUNTIME_API_KEY}`,
-            },
-        }
-        );
-        // console.log(`About to log value of "response"`)
-        // console.log(response)
-        return response.data;
-    } catch (error) {
-        if (error.response && error.response.data) {
-        return error.response.data;
-        } else {
-        console.error(error);
-        throw new Error("An unknown error occurred while fetching CSR data.");
-        }
+  const { HALOFUNTIME_API_KEY, HALOFUNTIME_API_URL } = process.env;
+
+  try {
+    const response = await axios.get(
+      `${HALOFUNTIME_API_URL}/halo-infinite/csr?gamertag=${encodeURIComponent(
+        xboxLiveGamertag
+      )}`,
+      {
+        headers: {
+          Authorization: `Bearer ${HALOFUNTIME_API_KEY}`,
+        },
+      }
+    );
+
+    return response.data;
+  } catch (error) {
+    if (error.response && error.response.data) {
+      return error.response.data;
+    } else {
+      console.error(error);
+      throw new Error("An unknown error occurred while fetching CSR data.");
     }
+  }
 }
 
 module.exports = getCSR;
