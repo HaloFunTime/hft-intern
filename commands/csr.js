3a95dfc140386cca09a7b70f0f31d2c136feb45a
michaelwdanko <michaeldanko@icloud.com>
2023-03-22 07:04:51 -0400

    Copies file contents from forked project

--- commands/csr.js
+++ commands/csr.js
@@ -1,91 +1,63 @@
-const { EmbedBuilder, SlashCommandBuilder } = require("discord.js");
-const axios = require("axios");
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const getCSR = require('../utils/getCSR');
+const buildRankEmbeds = require('../utils/buildRankEmbeds');
 
 module.exports = {
-  data: new SlashCommandBuilder()
-    .setName("csr")
-    .setDescription("Check the CSR for a gamertag")
-    .addStringOption((option) =>
-      option
-        .setName("gamertag")
-        .setDescription("A valid Xbox Live gamertag")
-        .setMaxLength(15)
-        .setRequired(true)
-    ),
-  async execute(interaction) {
-    const { HALOFUNTIME_API_KEY, HALOFUNTIME_API_URL } = process.env;
-    const gamertag = interaction.options.getString("gamertag");
-    const response = await axios
-      .get(
-        `${HALOFUNTIME_API_URL}/halo-infinite/csr?gamertag=${encodeURIComponent(
-          gamertag
-        )}`,
-        {
-          headers: {
-            Authorization: `Bearer ${HALOFUNTIME_API_KEY}`,
-          },
-        }
-      )
-      .then((response) => response.data)
-      .catch(async (error) => {
-        // Return the error payload directly if present
-        if (error.response.data) {
-          return error.response.data;
-        }
-        console.error(error);
-      });
-    if ("error" in response) {
-      if (response.error.details?.detail) {
-        await interaction.reply({
-          content: response.error.details.detail,
-          ephemeral: true,
-        });
-      } else {
-        await interaction.reply({
-          content: `An unknown error occurred while attempting to fetch CSR data for \`${gamertag}\`.`,
-          ephemeral: true,
-        });
-      }
-    } else {
-      const rankEmbeds = [];
-      for (let playlist of response.playlists) {
-        const currentCSR = playlist.current.csr;
-        // Only build an embed if the player has valid rank data
-        if (currentCSR !== -1) {
-          const currentTierDescription = playlist.current.tierDescription;
-          // Build the image name
-          const imageName = currentTierDescription
-            .replace(" ", "-")
-            .toLowerCase();
-          const rankEmbed = new EmbedBuilder()
-            .setTitle(playlist.playlistName)
-            .setThumbnail(
-              `https://api.halofuntime.com/static/csr_images/${imageName}.png`
-            )
-            .addFields({
-              name: `${currentTierDescription} (${currentCSR})`,
-              value: "**GT:** `" + response.gamertag + "`",
-            })
-            .setTimestamp()
-            .setFooter({
-              text: "Current CSR",
-            });
-          rankEmbeds.push(rankEmbed);
-        }
-      }
-      if (rankEmbeds.length === 0) {
-        let description = `\`${response.gamertag}\` is currently unranked.`;
-        if (response.gamertag === "HFT Intern") {
-          description +=
-            "\n\nThis is because quantifying my greatness is impossible. You mortals wouldn't understand.";
-        }
-        rankEmbeds.push(
-          new EmbedBuilder().setTitle("Unranked").setDescription(description)
-        );
-      }
-      await interaction.reply({
-        embeds: rankEmbeds,
-      });
-    }
-  },
+	data: new SlashCommandBuilder()
+		.setName('csr')
+		.setDescription("Get the CSR for a user by entering their gamertag.")
+		.addStringOption((option) =>
+		option
+		  .setName("gamertag")
+		  .setDescription("A valid Xbox Live gamertag")
+		  .setMaxLength(15)
+		  .setRequired(true)
+	  	),
+	async execute(interaction) {
+		try {
+			// console.log(`csr-slash.js - About to set const: gamertag`)
+			const gamertag = interaction.options.getString('gamertag');
+			console.log(`csr-slash.js - New value of const gamertag is:`)
+			console.log(gamertag)
+
+			// console.log(`csr-slash.js - About to set const: csr_response `)
+			const csr_response = await getCSR(gamertag);
+			console.log(`csr-slash.js - New value of const csr_response is:`)
+			// console.log(csr_response)
+
+			let response_embeds = []
+			
+			if ("error" in csr_response) {
+				console.log(`csr-slash.js - Attempting to check for an error in the csr_response`)
+				if (csr_response.error.details?.detail) {
+				  await interaction.reply({
+					content: csr_response.error.details.detail,
+					ephemeral: true,
+				  });
+				} else {
+				  await interaction.reply({
+					content: `An unknown error occurred while attempting to fetch CSR data for \`${gamertag}\`.`,
+					ephemeral: true,
+				  });
+				}
+			  } else {
+				console.log(`csr-slash.js - No error in csr_response. Attempting to set response_embeds to result of buildRankEmbeds`)
+				response_embeds = buildRankEmbeds(csr_response)
+			  }
+
+			//   console.log(`csr-slash.js - About to log value of response_embeds:`)
+			//   console.log(response_embeds)
+
+			await interaction.reply({
+				embeds: response_embeds
+			});
+
+		} catch (error) {
+			console.error(error);
+			await interaction.reply({
+				content: 'An error occurred while processing your request as a Discord slash command',
+				ephemeral: true,
+			});
+		}
+	},
 };
