b4d189b368fd17bfccd6104b6b618f77d2f811ca
Dylan Bohlender <dylan.bohlender@gmail.com>
2023-04-07 19:06:38 -0600

    Try update

--- customInteractions/seriesSelect.js
+++ customInteractions/seriesSelect.js
@@ -24,7 +24,7 @@ module.exports = {
           .setStyle(ButtonStyle.Primary);
       })
     );
-    await interaction.editReply({
+    await interaction.update({
       content: "",
       components: [row],
     });
