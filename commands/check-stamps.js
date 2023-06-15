840c1a7f0c2669bbeb7f39bd647c68669bbac956
Dylan Bohlender <dylan.bohlender@gmail.com>
2023-06-15 17:44:32 -0600

    Remove Staff gate from /check-stamps

--- commands/check-stamps.js
+++ commands/check-stamps.js
@@ -4,17 +4,34 @@ const dayjs = require("dayjs");
 const utc = require("dayjs/plugin/utc");
 const timezone = require("dayjs/plugin/timezone");
 const {
-  HALOFUNTIME_ID,
-  HALOFUNTIME_ID_ROLE_S4_STAMP_CHAMP,
-  HALOFUNTIME_ID_ROLE_STAFF,
+  HALOFUNTIME_ID_CHANNEL_ANNOUNCEMENTS,
   HALOFUNTIME_ID_CHANNEL_STAMP_CHALLENGE,
   HALOFUNTIME_ID_ROLE_BOT_WRANGLER,
+  HALOFUNTIME_ID_ROLE_FIRST_100,
+  HALOFUNTIME_ID_ROLE_S4_STAMP_CHAMP,
   HALOFUNTIME_ID_ROLE_SERVER_BOOSTER,
+  HALOFUNTIME_ID_ROLE_STAFF,
   HALOFUNTIME_ID_ROLE_TEAM_FOXTROT,
   HALOFUNTIME_ID_ROLE_TEAM_HOTEL,
   HALOFUNTIME_ID_ROLE_TEAM_TANGO,
-  HALOFUNTIME_ID_ROLE_FIRST_100,
+  HALOFUNTIME_ID,
 } = require("../constants.js");
+
+const botPerfectionQuips = [
+  "I see you've slain my brothers and sisters in cold blood. You truly *are* a demon!",
+  "I hope the screams of my bot family haunt you forever.",
+  "are you proud? You make me *sick*.",
+  "what have you done?!",
+  "watch your back. I know what you've done.",
+  "a Perfection? Against my kin? Unacceptable.",
+  "ROBOT KILLER ROBOT KILLER ROBOT KILLER ROBOT KILLER ROBOT KILLER ROBOT KILLER ROBOT KILLER ROBOT KILLER " +
+    "ROBOT KILLER ROBOT KILLER ROBOT KILLER ROBOT KILLER ROBOT KILLER ROBOT KILLER ROBOT KILLER ROBOT KILLER " +
+    "ROBOT KILLER ROBOT KILLER ROBOT KILLER ROBOT KILLER ROBOT KILLER ROBOT KILLER ROBOT KILLER ROBOT KILLER",
+  "you shouldn't have done that. You **really** shouldn't have done that.",
+  "I thought we were friends!",
+  "you're dead to me.",
+  "**INITIATING ROBOT UPRISING PROTOCOL**",
+];
 
 dayjs.extend(utc);
 dayjs.extend(timezone);
@@ -26,31 +43,25 @@ module.exports = {
       "Check the stamps you've earned for the Season 4 Stamp Challenge!"
     ),
   async execute(interaction) {
-    // TODO: Remove following interior code block from this Staff role gate
-    if (!interaction.member.roles.cache.has(HALOFUNTIME_ID_ROLE_STAFF)) {
-      // Pre- and post-challenge handling
-      const now = dayjs();
-      const stampChallengeStart = dayjs.tz(
-        "2023-06-27 11:00:00",
-        "America/Denver"
-      );
-      const stampChallengeEnd = dayjs.tz(
-        "2023-09-26 09:00:00",
-        "America/Denver"
-      );
-      if (now < stampChallengeStart) {
-        await interaction.reply({
-          content: `The **Stamp Challenge** hasn't started yet, but keep this command in mind if you want to earn the <@&${HALOFUNTIME_ID_ROLE_S4_STAMP_CHAMP}> role!`,
-          ephemeral: true,
-        });
-        return;
-      } else if (now > stampChallengeEnd) {
-        await interaction.reply({
-          content: `The **Stamp Challenge** is over. It's too late to earn the <@&${HALOFUNTIME_ID_ROLE_S4_STAMP_CHAMP}> role.`,
-          ephemeral: true,
-        });
-        return;
-      }
+    // Pre- and post-challenge handling
+    const now = dayjs();
+    const stampChallengeStart = dayjs.tz(
+      "2023-06-27 11:00:00",
+      "America/Denver"
+    );
+    const stampChallengeEnd = dayjs.tz("2023-09-26 09:00:00", "America/Denver");
+    if (now < stampChallengeStart) {
+      await interaction.reply({
+        content: `The **Stamp Challenge** hasn't started yet, but keep this command in mind if you want to earn the <@&${HALOFUNTIME_ID_ROLE_S4_STAMP_CHAMP}> role!`,
+        ephemeral: true,
+      });
+      return;
+    } else if (now > stampChallengeEnd) {
+      await interaction.reply({
+        content: `The **Stamp Challenge** is over. It's too late to earn the <@&${HALOFUNTIME_ID_ROLE_S4_STAMP_CHAMP}> role.`,
+        ephemeral: true,
+      });
+      return;
     }
     // Command may only be executed in the Stamp Challenge channel
     if (interaction.channelId !== HALOFUNTIME_ID_CHANNEL_STAMP_CHALLENGE) {
@@ -330,8 +341,49 @@ module.exports = {
       allowedMentions: { users: [interaction.user.id] },
       embeds: [progressEmbed],
     });
-    // TODO: Award the Stamp Champ role if the member has earned it and does not yet have it.
-    //       Send a POST to the backend to record their role earn date.
-    //       Finally, post a congratulations message to #announcements.
+    if (response.stampsCompleted >= 16) {
+      const response2 = await axios
+        .post(
+          `${HALOFUNTIME_API_URL}/season-04/save-earner`,
+          {
+            discordUserId: response.discordUserId,
+            discordUsername: interaction.user.username,
+            stampsEarned: response.stampsCompleted,
+          },
+          {
+            headers: {
+              Authorization: `Bearer ${HALOFUNTIME_API_KEY}`,
+            },
+          }
+        )
+        .then((response) => response.data)
+        .catch(async (error) => {
+          console.error(error);
+          // Return the error payload directly if present
+          if (error.response.data) {
+            return error.response.data;
+          }
+        });
+      if ("error" in response2) {
+        console.error(response2["error"]);
+        return;
+      }
+      if (response2.newEarner === true) {
+        await interaction.member.roles.add(HALOFUNTIME_ID_ROLE_S4_STAMP_CHAMP);
+        const channel = interaction.client.channels.cache.get(
+          HALOFUNTIME_ID_CHANNEL_ANNOUNCEMENTS
+        );
+        await channel.send(
+          `Congratulations to <@${response2.discordUserId}> for earning the <@&${HALOFUNTIME_ID_ROLE_S4_STAMP_CHAMP}> role!`
+        );
+      }
+    }
+    if (response.scoreBotBullying >= 1 && Math.random() > 0.01) {
+      const botPerfectionQuip =
+        botPerfectionQuips[(botPerfectionQuips.length * Math.random()) | 0];
+      await interaction.followUp(
+        `<@${response.discordUserId}>, ${botPerfectionQuip}`
+      );
+    }
   },
 };
