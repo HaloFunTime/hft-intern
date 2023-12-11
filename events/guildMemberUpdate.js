ba54af6a27e408bbb6127fccd88dc07071dd544c
Dylan Bohlender <dylan.bohlender@gmail.com>
2023-12-10 22:51:51 -0700

    Add and remove from threads on role change

--- events/guildMemberUpdate.js
+++ events/guildMemberUpdate.js
@@ -17,12 +17,63 @@ const {
 } = require("../constants");
 
 async function updateLfgThreadMembership(oldMember, newMember) {
-  // Get the role which has been added
-  const newRoles = newMember.roles.cache.filter(
-    (r) => !oldMember.roles.cache.has(r.id)
-  );
   const lfgChannel = oldMember.client.channels.cache.get(
     HALOFUNTIME_ID_CHANNEL_LFG
+  );
+  // Old Roles
+  const oldRoles = oldMember.roles.cache.filter(
+    (r) => !newMember.roles.cache.has(r.id)
+  );
+  oldRoles.forEach(async (role) => {
+    let thread;
+    let reason;
+    if (role.id === HALOFUNTIME_ID_ROLE_8S) {
+      thread = lfgChannel.threads.cache.find(
+        (t) => t.id === HALOFUNTIME_ID_CHANNEL_LFG_THREAD_8S_WITH_FRIENDS
+      );
+      reason = "Removed 8s Role";
+    } else if (role.id === HALOFUNTIME_ID_ROLE_BTB) {
+      thread = lfgChannel.threads.cache.find(
+        (t) => t.id === HALOFUNTIME_ID_CHANNEL_LFG_THREAD_BTB_AND_CHILL
+      );
+      reason = "Removed BTB Role";
+    } else if (role.id === HALOFUNTIME_ID_ROLE_CUSTOMS) {
+      thread = lfgChannel.threads.cache.find(
+        (t) => t.id === HALOFUNTIME_ID_CHANNEL_LFG_THREAD_CRAZY_CUSTOM_GAMES
+      );
+      reason = "Removed Customs Role";
+    } else if (role.id === HALOFUNTIME_ID_ROLE_PVE) {
+      thread = lfgChannel.threads.cache.find(
+        (t) => t.id === HALOFUNTIME_ID_CHANNEL_LFG_THREAD_FIREFIGHT_FUN
+      );
+      reason = "Removed PvE Role";
+    } else if (role.id === HALOFUNTIME_ID_ROLE_RANKED) {
+      thread = lfgChannel.threads.cache.find(
+        (t) => t.id === HALOFUNTIME_ID_CHANNEL_LFG_THREAD_RANKED_ARENA_GRIND
+      );
+      reason = "Removed Ranked Role";
+    } else if (role.id === HALOFUNTIME_ID_ROLE_SOCIAL) {
+      thread = lfgChannel.threads.cache.find(
+        (t) => t.id === HALOFUNTIME_ID_CHANNEL_LFG_THREAD_CASUAL_4V4_HALO
+      );
+      reason = "Removed Social Role";
+    } else if (role.id === HALOFUNTIME_ID_ROLE_TESTING) {
+      thread = lfgChannel.threads.cache.find(
+        (t) => t.id === HALOFUNTIME_ID_CHANNEL_LFG_THREAD_FORGE_MAP_TESTS
+      );
+      reason = "Removed Testing Role";
+    }
+    if (thread) {
+      try {
+        await thread.members.remove(newMember.id, reason);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  });
+  // New Roles
+  const newRoles = newMember.roles.cache.filter(
+    (r) => !oldMember.roles.cache.has(r.id)
   );
   newRoles.forEach(async (role) => {
     let thread;
@@ -64,7 +115,11 @@ async function updateLfgThreadMembership(oldMember, newMember) {
       reason = "Added Testing Role";
     }
     if (thread) {
-      await thread.members.add(newMember.id, reason);
+      try {
+        await thread.members.add(newMember.id, reason);
+      } catch (error) {
+        console.log(error);
+      }
     }
   });
 }
